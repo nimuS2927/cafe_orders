e5adb628e4be35ade67beb6471495df10ff4697f
Ivan Sumin <sumin.ivan01@mail.ru>
2025-03-27 01:30:59 +1000

    Добавить возможность множетсвенного выбора одного блюда в заказе. Добавить документацию.

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Product(models.Model):
+    """
+    Модель продукта (блюда) в кафе.
+    """
     STATUS_CHOICES = [
         ('available', 'Доступно'),
         ('unavailable', 'Недоступно'),
@@ -17,6 +20,9 @@ class Product(models.Model):
 
 
 class Order(models.Model):
+    """
+    Модель заказа, содержащая номер стола, список блюд, общую стоимость и статус.
+    """
     STATUS_CHOICES = [
         ('pending', 'В ожидании'),
         ('ready', 'Готово'),
@@ -25,14 +31,28 @@ class Order(models.Model):
 
     id = models.AutoField(primary_key=True)
     table_number = models.PositiveIntegerField(verbose_name="Номер стола")
-    items = models.ManyToManyField(Product, related_name="orders", verbose_name="Список блюд")
+    items = models.ManyToManyField(Product, through='OrderItem', verbose_name="Список блюд")
     total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Общая стоимость", default=0)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending', verbose_name="Статус")
 
-    def save(self, *args, **kwargs):
-        # Пересчитываем сумму перед сохранением
-        self.total_price = sum(product.price for product in self.items.all())
-        super().save(*args, **kwargs)
+    def calculate_total_price(self):
+        """
+        Пересчитывает общую стоимость заказа на основе товаров и их количества.
+        """
+        self.total_price = sum(item.product.price * item.quantity for item in self.order_items.all())
+        self.save()
 
     def __str__(self):
         return f"Заказ {self.id} (Стол {self.table_number}) - {self.get_status_display()}"
+
+
+class OrderItem(models.Model):
+    """
+    Промежуточная модель для связи "Многие ко многим" между Order и Product, добавляя поле "quantity".
+    """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items', verbose_name="Заказ")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Продукт")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+
+    def __str__(self):
+        return f"{self.product.name} x{self.quantity} в заказе {self.order.id}"
