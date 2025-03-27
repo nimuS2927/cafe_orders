78ba26c89f1cc067202648f364b0050d6c00a182
Ivan Sumin <sumin.ivan01@mail.ru>
2025-03-27 12:54:21 +1000

    Фиксы шаблонов. Добавить модели в админ панель

--- orders/admin.py
+++ orders/admin.py
@@ -1,2 +1,16 @@
 from django.contrib import admin
+from .models import Order, Product
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'status')
+    list_filter = ('status',)
+    search_fields = ('name',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'table_number', 'total_price', 'status')
+    list_filter = ('status',)
+    search_fields = ('table_number',)
